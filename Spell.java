2bb27b622a11c2124852ca741bf2f1c22d148cc0
Rhys Miller <sol0wingpixy@users.noreply.github.com>
2021-03-03 18:14:32 -0800

    Parser works

--- Spell.java
+++ Spell.java
@@ -1,61 +1,59 @@
-public class Spell implements Comparable<Spell>
+import org.jsoup.nodes.*;
+
+public class Spell
 {
-   public int level;
    public String name;
-   public boolean isDamage;
+   public String school;
+   public String level;//todo int
+   public String components;
+   public String castTime;
+   public String duration;
+   public String range;
+   public String aoeSize;
+   public String aoeType;
+   public String attackSave;
+   public String damageEffect;
+   public String text;
+   public Spell(String n,Element el)
+   {
+      name = n;
+      Element block = el.child(0).child(0);
+   
+      level = block.child(0).child(1).text();
+      castTime = block.child(1).child(1).text();
+      range = block.child(2).child(1).text();
+      if(block.child(2).child(1).childrenSize()>0)
+      {
+         aoeSize = block.child(2).child(1).child(0).text();
+         aoeType = block.child(2).child(1).child(0).child(0).attributes().get("class");
+      }
+      components = block.child(3).child(1).child(0).text();
+      duration = block.child(4).child(1).text();
+      school = block.child(5).child(1).text();
+      attackSave = block.child(6).child(1).text();
+      damageEffect = block.child(7).child(1).text();
+      text = el.child(0).child(2).text();
+   }
+   
    public Spell(String input)
    {
-      try{
-         String[] temp = input.split(",");
-         level = Integer.parseInt(temp[0]);
-         name = temp[1];
-         isDamage = temp[2].equals("1");
-      } catch(ArrayIndexOutOfBoundsException e)
-      {
-         isDamage = false;
-      }
+      String[] parts = input.split("_");
+      name = parts[0];
+      level = parts[1];
+      castTime = parts[2];
+      range = parts[3];
+      aoeSize = parts[4];
+      aoeType = parts[5];
+      components = parts[6];
+      duration = parts[7];
+      school = parts[8];
+      attackSave = parts[9];
+      damageEffect = parts[10];
+      text = parts[11];
    }
+   
    public String toString()
    {
-      return level + "," + name + "," + (isDamage?"1":"0");
-   }
-   
-   public String fancyToString()
-   {
-      String out = "";
-      switch(level)
-      {
-         case 0:
-            out += "Cantrip: ";
-            break;
-         case 1:
-            out += "1st: ";
-            break;
-         case 2:
-            out += "2nd: ";
-            break;
-         case 3:
-            out += "3rd: ";
-            break;
-         default:
-            out += level + "th: ";
-      }
-      return out + name;
-   }
-   
-   @Override
-   public boolean equals(Object o)
-   {
-      if(o instanceof Spell)
-      {
-         return ((Spell)(o)).name.equals(this.name);
-      }
-      return false;
-   }
-   public int compareTo(Spell s)
-   {  
-      if(this.level == s.level)
-         return this.name.compareTo(s.name);
-      return this.level - s.level;
+      return name+"_"+level+"_"+castTime+"_"+range+"_"+aoeSize+"_"+aoeType+"_"+components+"_"+duration+"_"+school+"_"+attackSave+"_"+damageEffect+"_"+text;
    }
 }
---
+++ Spellbase.java
@@ -0,0 +1,86 @@
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import java.io.*;
+import java.util.*;
+
+public class Spellbase
+{
+   private static final String FILENAME = "Database.txt";
+
+   public static void main(String[] args)
+   {
+      ArrayList<Spell> spellList = new ArrayList();
+      Scanner input = new Scanner(System.in);
+      System.out.println("Read New Data? (y/n)");
+      if(input.nextLine().equals("y"))
+      {
+         for(int i = 1; i <= 26; i++)
+         {
+            extractSpells(i,spellList);
+         }
+      }
+      else
+      {
+         readFromFile(spellList);
+      }
+      
+      //*
+      outputToFile(spellList);
+      //*/
+   }
+   
+   public static void readFromFile(ArrayList<Spell> spellList)
+   {
+      try
+      {
+         File inputFile = new File(FILENAME);
+         Scanner input = new Scanner(inputFile);
+         while(input.hasNextLine())
+         {
+            spellList.add(new Spell(input.nextLine()));
+         }
+         input.close();
+      }
+      catch(IOException e)
+      {
+         e.printStackTrace();   
+      }
+   }
+   
+   public static void outputToFile(ArrayList<Spell> spellList)
+   {
+      try
+      {
+         FileWriter output = new FileWriter(FILENAME);
+         for(int i = 0; i < spellList.size(); i++)
+         {
+            output.write(spellList.get(i).toString()+"\n");
+         }
+         output.close();
+      }
+      catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+   
+   public static void extractSpells(int num, ArrayList<Spell> spellList)
+   {
+      try
+      {
+         File input = new File("spell-pages/spell-page"+num+".html");
+         Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+         Element curr = doc.child(0).child(1).child(0);
+         
+         for(int i = 1; i < curr.childrenSize(); i+=2)
+         {
+            spellList.add(new Spell(curr.child(i-1).child(2).child(0).text(),curr.child(i)));
+         }
+      }
+      catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+   
+}
