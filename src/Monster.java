d9d3e830ba3f63f330ff62576e53435589426ca8
Rhys Miller <rhyspmiller@gmail.com>
2022-01-17 10:59:34 -0800

    Avg Dmg calcs, search/sort options
    Big commit, hadn't been updating properly. All monster data has been pulled from DnDBeyond by another script, kept private (because i don't own it). Added many differnt filter options, mainly based around Conjure Animals @ 3rd level. Also have some light language parsing to determine average damage, including parsing many multiattack features to determine average damage.

--- src/Monster.java
+++ src/Monster.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Monster implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final String[] statNames = new String[] {"STR","DEX","CON","INT","WIS","CHA"};
 
 	boolean hasInfo;
@@ -23,16 +23,23 @@ public class Monster implements Serializable {
 	public int[] stats;
 	public int[] saves;
 	public String skills;// split it up
+	public String dmgVul;
 	public String dmgRes;
 	public String dmgImm;
 	public String condImm; // fix all these?
 	public String senses;
 	public String languages;
+	public boolean hasToHit;
 	public int toHitMod;//todo; just grabs 1st action, no spell attack or other actions
+	public String fullText;
+	public List<String> attacks;
+	public double avgDamage;
 
 	public Monster(Element smolInfo, Element bigInfo) {
 		String tempString;
 		int tempInt;
+
+		fullText = bigInfo.child(0).child(0).wholeText();
 
 		name = smolInfo.child(2).child(0).text();
 
@@ -45,71 +52,110 @@ public class Monster implements Serializable {
 			cr = -10;
 
 		type = Type.toType(smolInfo.child(3).text());
+		try {
 
-		if (bigInfo.child(0).child(0).className().equals("ddb-blocked-content")) {
-			hasInfo = false;
 
-			source = bigInfo.child(0).child(0).child(0).child(1).child(0).text();
-		} else {
-			hasInfo = true;
+			if (bigInfo.child(0).child(0).className().equals("ddb-blocked-content")) {
+				hasInfo = false;
 
-			source = bigInfo.child(1).child(bigInfo.child(1).childrenSize() - 1).text();
-
-			tempString = bigInfo.child(0).child(0).child(0).child(1).text();
-			tempInt = tempString.indexOf(" ");
-			size = tempString.substring(0, tempInt);
-
-			if (tempString.contains("(")) {
-				subtype = tempString.substring(tempString.indexOf("(") + 1, tempString.indexOf(")"));
-			}
-
-			tempString = bigInfo.child(0).child(0).child(2).child(0).child(1).text();
-			if (tempString.indexOf(" ") != -1) {
-				tempString = tempString.substring(0, tempString.indexOf(" "));
-			}
-			ac = Integer.parseInt(tempString);
-
-			hp = Integer.parseInt(bigInfo.child(0).child(0).child(2).child(1).child(1).child(0).text());
-
-			tempString = bigInfo.child(0).child(0).child(2).child(1).child(1).child(1).text();
-			tempInt = tempString.indexOf("d");
-			hdCount = Integer.parseInt(tempString.substring(1, tempInt));
-
-			if (tempString.indexOf(" ") != -1) {
-				hdSize = Integer.parseInt(tempString.substring(tempInt + 1, tempString.indexOf(" ")));
+				source = bigInfo.child(0).child(0).child(0).child(1).child(0).text();
 			} else {
-				hdSize = Integer.parseInt(tempString.substring(tempInt + 1, tempString.indexOf(")")));
-			}
+				hasInfo = true;
 
-			speeds = bigInfo.child(0).child(0).child(2).child(2).text();
+				source = bigInfo.child(1).child(bigInfo.child(1).childrenSize() - 1).text();
 
-			stats = new int[6];
-			for (int i = 0; i < stats.length; i++) {
-				stats[i] = Integer
-						.parseInt(bigInfo.child(0).child(0).child(3).child(1).child(i).child(1).child(0).text());
-			}
+				tempString = bigInfo.child(0).child(0).child(0).child(1).text();
+				tempInt = tempString.indexOf(" ");
+				size = tempString.substring(0, tempInt);
 
-			for (int i = 0; i < bigInfo.child(0).child(0).child(4).childrenSize() - 1; i++) {
-				tidbitsParsing(bigInfo.child(0).child(0).child(4).child(i));
-			}
-			if(saves == null) {
-				saves = new int[6];
-				for(int i=0; i<stats.length; i++) {
-					tempInt = stats[i]-10;
-					if(tempInt < 0)
-						tempInt--;
-					saves[i] = tempInt/2;
+				if (tempString.contains("(")) {
+					subtype = tempString.substring(tempString.indexOf("(") + 1, tempString.indexOf(")"));
+				}
+
+				tempString = bigInfo.child(0).child(0).child(2).child(0).child(1).text();
+
+				if (tempString.indexOf(" ") != -1) {
+					tempString = tempString.substring(0, tempString.indexOf(" "));
+				}
+				ac = Integer.parseInt(tempString);
+
+				hp = Integer.parseInt(bigInfo.child(0).child(0).child(2).child(1).child(1).child(0).text());
+
+				tempString = bigInfo.child(0).child(0).child(2).child(1).child(1).child(1).text();
+				tempInt = tempString.indexOf("d");
+				hdCount = Integer.parseInt(tempString.substring(1, tempInt));
+
+				if (tempString.indexOf(" ") != -1) {
+					hdSize = Integer.parseInt(tempString.substring(tempInt + 1, tempString.indexOf(" ")));
+				} else {
+					hdSize = Integer.parseInt(tempString.substring(tempInt + 1, tempString.indexOf(")")));
+				}
+
+				speeds = bigInfo.child(0).child(0).child(2).child(2).text();
+
+				stats = new int[6];
+				for (int i = 0; i < stats.length; i++) {
+					stats[i] = Integer
+							.parseInt(bigInfo.child(0).child(0).child(3).child(1).child(i).child(1).child(0).text());
+				}
+
+				for (int i = 0; i < bigInfo.child(0).child(0).child(4).childrenSize() - 1; i++) {
+					tidbitsParsing(bigInfo.child(0).child(0).child(4).child(i));
+				}
+				if(saves == null) {
+					saves = new int[6];
+					for(int i=0; i<stats.length; i++) {
+						tempInt = stats[i]-10;
+						if(tempInt < 0)
+							tempInt--;
+						saves[i] = tempInt/2;
+					}
+				}
+				for (int i = 0; i < bigInfo.child(0).child(0).child(6).childrenSize(); i++) {
+					if (bigInfo.child(0).child(0).child(6).child(i).child(0).text().equals("Actions")) {
+
+						//tempString = bigInfo.child(0).child(0).child(6).child(i).child(1).text();
+						Element curr = bigInfo.child(0).child(0).child(6).child(i).child(1);
+						if(name.equals("Dolgrim"))
+							tempInt = 0;
+						attacks = new ArrayList<String>();
+						for(Element j : curr.children())
+						{
+							attacks.add(j.text());
+						}
+						tempInt = -1;
+						int j;
+						for(j=0;j<attacks.size();j++)
+						{
+							tempInt = attacks.get(j).indexOf("+");	
+							if(tempInt != -1)
+								break;
+						}
+						if(j == attacks.size())
+						{
+							hasToHit = false;
+						}
+						else
+						{
+							tempString = attacks.get(j).substring(tempInt,  attacks.get(j).indexOf(" ", tempInt));
+							if(tempString.length() > 1)
+							{
+								toHitMod = Integer.parseInt(tempString);
+								hasToHit = true;
+								avgDamage = calcAvgDamage();
+								break;
+							}
+							else
+								hasToHit = false;
+						}
+					}
 				}
 			}
-			for (int i = 0; i < bigInfo.child(0).child(0).child(6).childrenSize(); i++) {
-				if (bigInfo.child(0).child(0).child(6).child(i).child(0).text().equals("Actions")) {
-					tempString = bigInfo.child(0).child(0).child(6).child(i).child(1).text();
-					tempInt = tempString.indexOf("+");
-					tempString = tempString.substring(tempInt, tempString.indexOf(" ", tempInt));
-					toHitMod = Integer.parseInt(tempString);
-					break;
-				}
-			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(name);
+			e.printStackTrace();
 		}
 	}
 
@@ -135,6 +181,8 @@ public class Monster implements Serializable {
 		case "Skills":
 			skills = curr.child(1).text();
 			break;
+		case "Damage Vulnerabilities":
+			dmgVul = curr.child(1).text();
 		case "Damage Resistances":
 			dmgRes = curr.child(1).text();
 			break;
@@ -151,7 +199,255 @@ public class Monster implements Serializable {
 			languages = curr.child(1).text();
 			break;
 		default:
-			System.out.println("Add this to Monster.java: " + curr.child(0).text());
+			System.out.println("Add this to Monster.java: " + curr.child(0).text() + ", from: " + name);
 		}
+	}
+	public String toString()
+	{
+		return name + " (CR:" + cr + ", HP:" + Monsterbase.convertHP(this) + ", DPRtoHit: " + Monsterbase.avgDmgToHit(this) + ")\n";
+	}
+	
+	public Integer sizeNumber()
+	{
+		switch(size)
+		{
+		case "Tiny":
+			return 1;
+		case "Small":
+			return 2;
+		case "Medium":
+			return 3;
+		case "Large":
+			return 4;
+		case "Huge":
+			return 5;
+		case "Gargantuan":
+			return 6;
+		default:
+			return 0;
+		}
+	}
+
+	public double calcAvgDamage()
+	{
+		String first = attacks.get(0);
+		if(name.equals("Chimera"))
+			first = first;
+
+		if(first.contains("Multiattack"))
+		{
+			int index = first.indexOf("two");
+			int offset = 4;
+			int mult = 2;
+			if(index ==-1)
+			{
+				index = first.indexOf("three");
+				offset = 6;
+				mult = 3;
+				if(index ==-1)
+				{
+					index = first.indexOf("four");
+					offset = 5;
+					mult = 4;
+					if(index ==-1)
+					{
+						index = first.indexOf("five");
+						offset = 5;
+						mult = 5;
+					}
+				}
+			}
+
+			int nextIndex = first.indexOf(" ",index+offset);
+			if(nextIndex < 0)
+			{
+				if(first.substring(index+offset).equals("attacks."))
+				{
+					int attacksLeft = mult;
+					int attackIndex = 1;
+					double damageTotal = 0;
+					while(attacksLeft > 0)
+					{
+						damageTotal += calcDamage(attacks.get(attackIndex++));
+						if(attackIndex == attacks.size())
+							attackIndex = 1;
+						attacksLeft--;
+					}
+					return damageTotal;
+				}
+				else
+				{
+					System.out.println();
+					return -1;
+				}
+			}
+			String name = first.substring(index+offset,nextIndex);
+			if(name.equals("attacks:"))
+			{
+				String[] split = first.split(" with its ");
+				String[] fullSplit = first.split(" with its ");
+				double totalDamage = 0;
+				for(int i = 1; i < split.length; i++)
+				{
+					//TODO, reference previous element. At the end there will be a number word
+					//it indicates how many attacks are made i.e. "one] with its [claws."
+					int splitIndex = split[i].length();
+					if(split[i].indexOf('.') != -1)
+						splitIndex = Math.min(splitIndex, split[i].indexOf('.'));
+					if(split[i].indexOf(',') != -1)
+						splitIndex = Math.min(splitIndex, split[i].indexOf(','));
+					if(split[i].indexOf(' ') != -1)
+						splitIndex = Math.min(splitIndex, split[i].indexOf(' '));
+
+					split[i] = split[i].substring(0,splitIndex);
+
+					for(int j = 1; j < attacks.size(); j++)
+					{
+						if(attacks.get(j).toLowerCase().startsWith(split[i].toLowerCase()))
+						{
+							if(i == attacks.size())
+								break;
+							double temp = calcDamage(attacks.get(i));
+							if(fullSplit[i-1].endsWith("one"))
+								totalDamage += temp;
+							else if (fullSplit[i-1].endsWith("two"))
+								totalDamage += temp*2;
+							else if (fullSplit[i-1].endsWith("three"))
+								totalDamage += temp*3;
+							else if (fullSplit[i-1].endsWith("four"))
+								totalDamage += temp*4;
+						}
+					}
+				}
+				if(mult == 5)
+					System.out.println(this.name + " ; "+totalDamage);
+				return totalDamage;
+			}
+			else
+			{
+				for(int i = 1; i < attacks.size(); i++)
+				{
+					if(attacks.get(i).toLowerCase().startsWith(name.toLowerCase()))
+					{
+						return calcDamage(attacks.get(i))*mult;
+					}
+				}
+				return -1;
+			}
+		}
+		else
+		{
+			return calcDamage(attacks.get(0));
+		}
+	}
+
+	public double calcDamage(String attack)
+	{
+		if(name.equals("Giant Poisonous Snake"))
+			attack = attack;
+		List<String> damageStrings = getInParen(attack);
+		double total = 0;
+		int index;
+		if(damageStrings.size()==0)
+		{
+			index = attack.indexOf("poison");
+			if(index == -1)
+				index = attack.indexOf("slashing");
+			if(index == -1)
+				index = attack.indexOf("bludgeoning");
+			if(index == -1)
+				index = attack.indexOf("piercing");
+			if(index == -1)
+				index = attack.indexOf("force");
+			if(index == -1)
+				return -1;
+			index--;
+			do {
+				index--;
+			} while (attack.charAt(index)!=' ');
+			index++;
+			String offDamage = attack.substring(index,attack.indexOf(' ',index));
+			if(offDamage.matches("[0-9]+"))
+				total += Integer.parseInt(offDamage);
+		}
+		else
+		{
+			boolean posMod = true;
+			for(int j = 0; j < damageStrings.size(); j++)
+			{
+				String s = damageStrings.get(j).replace("plus","+");
+				String[] split;
+				if(s.contains("+"))
+				{
+					split = s.split("\\+");
+				}
+				else
+				{
+					if(s.contains("−"))
+					{
+						split = s.split("\\−");
+						posMod = false;
+					}
+					else
+					{
+						if(s.contains("-"))
+						{
+							split = s.split("\\-");
+							posMod = false;
+						}
+						else
+						{
+							if(s.contains("–"))
+							{
+								split = s.split("\\–");
+								posMod = false;
+							}
+							else
+								split = new String[]{s};
+						}
+					}
+				}
+				for(int i = 0; i < split.length; i++)
+				{
+					split[i] = split[i].trim();
+					index = split[i].indexOf("d");
+					if(index!=-1 && !split[i].startsWith("d") && Character.isDigit(split[i].charAt(index-1)) && !split[i].endsWith("d") && Character.isDigit(split[i].charAt(index+1)) )
+					{
+						int dieNum = Integer.parseInt(split[i].split("d")[0]);
+						int dieType = Integer.parseInt(split[i].split("d")[1]);
+						if(j == damageStrings.size()-1 && attack.contains("half"))
+							total += dieNum * (((double)dieType + 1) * 0.5) * 0.5;
+						else
+							total += dieNum * (((double)dieType + 1) * 0.5);
+					}
+					else
+					{
+						if(split[i].matches("[0-9]+"))
+						{
+							if(posMod)
+								total += Integer.parseInt(split[i]);
+							else
+								total -= Integer.parseInt(split[i]);
+						}
+					}
+				}
+			}
+		}
+		
+		return total;
+	}
+
+	private static List<String> getInParen(String text)
+	{
+		List<String> vals = new ArrayList<String>();
+		int startIndex;
+		int endIndex;
+		while(text.indexOf('(') != -1) {
+			startIndex = text.indexOf('(');
+			endIndex = text.indexOf(')');
+			vals.add(text.substring(startIndex+1,endIndex));
+			text = text.substring(endIndex+1);
+		}
+		return vals;
 	}
 }
--- src/Monsterbase.java
+++ src/Monsterbase.java
@@ -2,23 +2,26 @@ import org.jsoup.*;
 import org.jsoup.nodes.*;
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Monsterbase
 {
 	private static final String FILENAME = "MonsterDatabaseSerial.txt";
-	
+
 	private static Scanner input;
-	
+	private static final int BASECR = 2;
+	private static final int BASEAC = 17;
+
 	public static void main(String[] args)
 	{
-		ArrayList<Monster> monsterList = new ArrayList<Monster>();
+		List<Monster> monsterList = new ArrayList<Monster>();
 		//Reading new Data:
-		
+
 		input = new Scanner(System.in);
 		//System.out.println("Read New Data? (y/n)");
 		if(/*input.nextLine().equals("y")*/true)
 		{		
-			for(int i=1;i<=2;i++) {
+			for(int i=1;i<=106;i++) {
 				extractMonsters(i,monsterList);	
 			}
 		}
@@ -26,21 +29,235 @@ public class Monsterbase
 		{
 			monsterList=readFromFile();
 		}
-		double avgWisSave = 0;
-		for(int i=0; i<monsterList.size(); i++)
+		
+		displayAC(monsterList,11,11);
+		
+		monsterList = filterBy(monsterList,"hasInfo","True");
+		monsterList = filterBy(monsterList,"Type","Beast");
+		monsterList = filterBy(monsterList,"Trim","Swarm of");
+		monsterList = filterBy(monsterList,"CR","-4 2");
+		//monsterList = filterBy(monsterList,"SearchSpeed","swim");
+		//monsterList = sortBy(monsterList,"Hybrid","Dec");
+		//monsterList = sortBy(monsterList,"AvgDmgAdj","Dec");
+		//monsterList = sortBy(monsterList,"AvgDmgToHit","Dec");
+		monsterList = sortBy(monsterList,"ConAniHP","Dec");
+		monsterList = sortBy(monsterList, "Size","Dec");
+		//monsterList = sortBy(monsterList, "HPAC","Dec");
+		//monsterList = sortBy(monsterList,"toHit","Asc");
+		//monsterList = sortBy(monsterList,"HPSaves","Dec");
+		//monsterList = sortBy(monsterList,"CR","Dec");
+		//monsterList = filterBy(monsterList,"Search","magic");
+		for(Monster m : monsterList)
 		{
-			if(monsterList.get(i).hasInfo && monsterList.get(i).cr <= 5) {
-				System.out.println(monsterList.get(i).name + " : " + monsterList.get(i).saves[4]);
-				avgWisSave += monsterList.get(i).saves[4];
-			}
+			System.out.println("- "+m.name + " | " +m.size +", CR:"+m.cr+", HP:"+convertHP(m)+", DPRtoHit:"+avgDmgToHit(m)+", AdjHP:"+avgHPAC(m));
 		}
-		avgWisSave /= monsterList.size();
-		System.out.println("Avg wis save: " + avgWisSave);
 		outputToFile(monsterList);
-		System.out.println("didn't break");
 	}
 
+	private static List<Monster> sortBy(List<Monster> monsterList, String sortType, String sorter)
+	{
+		switch(sortType)
+		{
+		case "ConAniHP":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> convertHP(m2).compareTo(convertHP(m1))).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> convertHP(m1).compareTo(convertHP(m2))).collect(Collectors.toList());
+			return monsterList;
+		case "HPSaves":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> avgHPSaves(m2).compareTo(avgHPSaves(m1))).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> avgHPSaves(m1).compareTo(avgHPSaves(m2))).collect(Collectors.toList());
+			return monsterList;
+		case "HPAC":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> avgHPAC(m2).compareTo(avgHPAC(m1))).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> avgHPAC(m1).compareTo(avgHPAC(m2))).collect(Collectors.toList());
+			return monsterList;
+		case "toHit":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> new Integer(m2.toHitMod).compareTo(m1.toHitMod)).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> new Integer(m1.toHitMod).compareTo(m2.toHitMod)).collect(Collectors.toList());
+			return monsterList;
+		case "AvgDmg":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> new Double(m2.avgDamage).compareTo(m1.avgDamage)).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> new Double(m1.avgDamage).compareTo(m2.avgDamage)).collect(Collectors.toList());
+			return monsterList;
+		case "CR":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> new Integer(m2.cr).compareTo(m1.cr)).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> new Integer(m1.cr).compareTo(m2.cr)).collect(Collectors.toList());
+			return monsterList;
+		case "Size":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> m2.sizeNumber().compareTo(m1.sizeNumber())).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> m1.sizeNumber().compareTo(m2.sizeNumber())).collect(Collectors.toList());
+			return monsterList;
+		case "AvgDmgAdj":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> convertAvgDmg(m2).compareTo(convertAvgDmg(m1))).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> convertAvgDmg(m1).compareTo(convertAvgDmg(m2))).collect(Collectors.toList());
+			return monsterList;
+		case "AvgDmgToHit":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> avgDmgToHit(m2).compareTo(avgDmgToHit(m1))).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> avgDmgToHit(m1).compareTo(avgDmgToHit(m2))).collect(Collectors.toList());
+			return monsterList;
+		case "Hybrid":
+			if(sorter.equals("Dec"))
+				monsterList = monsterList.stream().sorted((m1,m2) -> HybridAvg(m2).compareTo(HybridAvg(m1))).collect(Collectors.toList());
+			else
+				monsterList = monsterList.stream().sorted((m1,m2) -> HybridAvg(m1).compareTo(HybridAvg(m2))).collect(Collectors.toList());
+			return monsterList;
+		default:
+			return monsterList;
+		}
+	}
 	
+	public static Double HybridAvg(Monster m)
+	{
+		double avgDmg = Monsterbase.avgDmgToHit(m);
+		double avgHp = (double)Monsterbase.convertHP(m);
+		double result = 1.0*avgDmg + 0.5*avgHp;
+		return result;
+	}
+	
+	public static Double avgDmgToHit(Monster m)
+	{
+		int toHit = m.toHitMod;
+		double avgDmg = Monsterbase.convertAvgDmg(m);
+		double result = 0;
+		result += avgDmg*0.075; // nat 20, 5% * 1.5 damage
+		int neededToHit = BASEAC - toHit;
+		if(neededToHit < 20)
+		{
+		if(neededToHit < 1)
+			result += avgDmg*.95;
+		else
+			result += avgDmg * (0.05 * (20-neededToHit));
+		}
+		return result;
+	}
+	
+	public static Double avgHPAC(Monster m)
+	{
+		double convertHP = Monsterbase.convertHP(m);
+		double ACfraction = 1 + .025*(m.ac-10);
+		return ACfraction * convertHP;
+	}
+	
+	public static Double avgHPSaves(Monster m)
+	{
+		double convertHP = Monsterbase.convertHP(m);
+		double rollingDefense = .05*m.ac;
+		rollingDefense += .025*m.saves[1];
+		rollingDefense += .025*m.saves[2];
+		rollingDefense += .025*m.saves[4];
+		rollingDefense /= 7;
+		return rollingDefense * convertHP;
+	}
+
+	public static Double convertAvgDmg(Monster m)
+	{
+		int cr = m.cr;
+		double dmg = m.avgDamage;
+		double tempCR = cr;
+		if (tempCR < 0)
+			tempCR = -1 / tempCR;
+		double ratio = BASECR / tempCR;
+		double result = (ratio * dmg);
+		return result;
+	}
+
+	public static Integer convertHP(Monster m)
+	{
+		int cr = m.cr;
+		int hp = m.hp;
+		double tempCR = cr;
+		if (tempCR < 0)
+			tempCR = -1 / tempCR;
+		double ratio = BASECR / tempCR;
+		int result = (int)(ratio * hp);
+		return result;
+	}
+
+	private static List<Monster> filterBy(List<Monster> monsterList, String filterType, String filter)
+	{
+		switch(filterType)
+		{
+		case "hasInfo":
+			monsterList = monsterList.stream().filter(m -> m.hasInfo == Boolean.parseBoolean(filter)).collect(Collectors.toList());
+			return monsterList;
+		case "Type":
+			monsterList = monsterList.stream().filter(m -> m.type == Type.toType(filter)).collect(Collectors.toList());
+			return monsterList;
+		case "CR":
+			String[] temp = filter.split(" ");
+			int[] minAndMax = {Integer.parseInt(temp[0]),Integer.parseInt(temp[1])};
+			monsterList = monsterList.stream().filter(m -> m.cr >= minAndMax[0] && m.cr <= minAndMax[1]).collect(Collectors.toList());
+			return monsterList;
+		case "Trim":
+			monsterList = monsterList.stream().filter(m -> !m.name.contains(filter)).collect(Collectors.toList());
+			return monsterList;
+		case "Search":
+			monsterList = monsterList.stream().filter(m -> m.fullText.toLowerCase().contains(filter.toLowerCase())).collect(Collectors.toList());
+			return monsterList;
+		case "SearchSpeed":
+			monsterList = monsterList.stream().filter(m -> m.speeds.toLowerCase().contains(filter.toLowerCase())).collect(Collectors.toList());
+			return monsterList;
+		default:
+			return monsterList;
+		}
+	}
+
+
+
+	private static void displaySaves(List<Monster> monsterList, int crMin, int crMax)
+	{
+		double[] avgSaves = new double[6];
+		int count = 0;
+		for(int i=0; i<monsterList.size(); i++)
+		{
+			if(monsterList.get(i).hasInfo && monsterList.get(i).cr >= crMin && monsterList.get(i).cr <= crMax) {
+				for(int j=0;j<6;j++)
+				{
+					avgSaves[j] += monsterList.get(i).saves[j];
+				}
+				count++;
+			}
+		}
+		System.out.println("Saves for creatures between CR " + crMin +", " + crMax);
+		System.out.println("Avg Str Save: "+avgSaves[0]/count);
+		System.out.println("Avg Dex Save: "+avgSaves[1]/count);
+		System.out.println("Avg Con Save: "+avgSaves[2]/count);
+		System.out.println("Avg Int Save: "+avgSaves[3]/count);
+		System.out.println("Avg Wis Save: "+avgSaves[4]/count);
+		System.out.println("Avg Cha Save: "+avgSaves[5]/count);
+	}
+	
+	private static void displayAC(List<Monster> monsterList, int crMin, int crMax)
+	{
+		double avgAC = 0;
+		int count = 0;
+		for(int i=0; i<monsterList.size(); i++)
+		{
+			if(monsterList.get(i).hasInfo && monsterList.get(i).cr >= crMin && monsterList.get(i).cr <= crMax) {
+				avgAC += monsterList.get(i).ac;
+				count++;
+			}
+		}
+		System.out.println("AC for creatures between CR " + crMin +", " + crMax + " is: " + avgAC/count);
+	}
+
 	private static int getResponse(String q,String[] ans){
 		System.out.println(q);
 		System.out.println("0: Done");
@@ -50,13 +267,13 @@ public class Monsterbase
 		int i = Integer.parseInt(input.nextLine());
 		return i;
 	}
-	public static ArrayList<Monster> readFromFile()
+	public static List<Monster> readFromFile()
 	{
 		try
 		{
 			FileInputStream inputFile = new FileInputStream(FILENAME);
 			ObjectInputStream objIn = new ObjectInputStream(inputFile);
-			ArrayList<Monster> readSpellList = (ArrayList<Monster>)objIn.readObject();
+			List<Monster> readSpellList = (ArrayList<Monster>)objIn.readObject();
 			objIn.close();
 			inputFile.close();
 			return readSpellList;
@@ -68,7 +285,7 @@ public class Monsterbase
 		}
 	}
 
-	public static void outputToFile(ArrayList<Monster> monsterList)
+	public static void outputToFile(List<Monster> monsterList)
 	{
 		try
 		{
@@ -84,15 +301,15 @@ public class Monsterbase
 		}
 	}
 
-	public static void extractMonsters(int num, ArrayList<Monster> monsterList)
+	public static void extractMonsters(int num, List<Monster> monsterList)
 	{
 		try
 		{
-			File input = new File("src/monster-pages/monster-page"+num+".html");
+			File input = new File("src/Monster-Pages/Monster-Page"+num+".html");
 			Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
-			
-			Element curr = doc.child(0).child(1).child(0);
-			
+
+			Element curr = doc.child(0).child(1).child(0).child(0);
+
 			for(int i = 0; i < curr.childrenSize(); i+=2)
 			{
 				monsterList.add(new Monster(curr.child(i),curr.child(i+1)));
